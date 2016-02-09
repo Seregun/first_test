4098584c4cbcca6bd8b8bc15653fa8c95fcfcfb5
serg <you@example.com>
2016-02-09 22:03:13 +0200

    Виправлення візуального виду коду в urls.py

--- Stepashko/urls.py
+++ Stepashko/urls.py
@@ -23,4 +23,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL,
-            document_root=settings.STATIC_ROOT)
+           document_root=settings.STATIC_ROOT)
